b8f78fff80c9e9e966d936c5de788d9e9b7d6254
Jason-lab1126 <66370735+Jason-lab1126@users.noreply.github.com>
2024-08-05 21:24:23 -0700

    Adding my first comment with git

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -4,6 +4,7 @@ import com.houarizegai.calculator.ui.CalculatorUI;
 
 public class App {
 
+    //This is my first comment
     public static void main(String[] args) {
         new CalculatorUI();
     }
